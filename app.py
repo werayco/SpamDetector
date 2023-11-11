4c5e2ad68ef91928e92ccd5ef74851751b886a65
werayco <werayco@gmail.com>
2023-11-11 07:18:34 -0800

    Updated Nov 11

--- app.py
+++ app.py
@@ -15,36 +15,36 @@ def index():
     return render_template("index.html")
 
 stemmer = PorterStemmer()
-# def removerr(y):
-#         clean_message = re.sub("[^a-zA-z]"," ", y)
-#         clean_message = clean_message.lower()
-#         clean_message = clean_message.split()
-#         clean_message = [stemmer.stem(everyword) for everyword in clean_message if not everyword in stopwords.words("english")]
-#         clean_message = " ".join(clean_message)
-#         return clean_message
+def removerr(y):
+        clean_message = re.sub("[^a-zA-z]"," ", y)
+        clean_message = clean_message.lower()
+        clean_message = clean_message.split()
+        clean_message = [stemmer.stem(everyword) for everyword in clean_message if not everyword in stopwords.words("english")]
+        clean_message = " ".join(clean_message)
+        return clean_message
 
 
-# @app.route("/spam_classifier",methods=["GET", "POST"])
-# def classifier():
-#    if request.method == "POST":
-#         data = request.form.get("input")
-#         clean_message = re.sub("[^a-zA-z]"," ", data)
-#         clean_message = clean_message.lower()
-#         clean_message = clean_message.split()
-#         clean_message = [stemmer.stem(everyword) for everyword in clean_message if not everyword in stopwords.words("english")]
-#         convt_text = vec.fit_transform(clean_message)
-#         prediction = model_name.predict(convt_text)
-#         return f"<h1> preprocessed {prediction}</h1>"
+@app.route("/spam_classifier",methods=["GET", "POST"])
+def classifier():
+   if request.method == "POST":
+        data = request.form.get("input")
+        clean_message = re.sub("[^a-zA-z]"," ", data)
+        clean_message = clean_message.lower()
+        clean_message = clean_message.split()
+        clean_message = [stemmer.stem(everyword) for everyword in clean_message if not everyword in stopwords.words("english")]
+        convt_text = vec.fit_transform(clean_message)
+        prediction = model_name.predict(convt_text)
+        return f"<h1> preprocessed {prediction}</h1>"
 
-# if __name__ == "__main__":
-#     app.run(debug=True)
+if __name__ == "__main__":
+    app.run(debug=True)
 
 
-# @app.route("/predict",methods=["POST","GET"])
-# def predictor()->str:
-#     if request.method=="POST":
-#      data1 = request.form.get("input")
-#      conv=vec.fit_transform()
+@app.route("/predict",methods=["POST","GET"])
+def predictor()->str:
+    if request.method=="POST":
+     data1 = request.form.get("input")
+     conv=vec.fit_transform()
 
 
 x = 1
